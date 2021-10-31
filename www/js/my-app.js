6b91292496e4c9a4c64e32c3baa7f6f3aca6a5a3
unknown <antubda@gmail.com>
2021-10-31 20:56:03 -0300

    31/10 +recetas

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -21,7 +21,7 @@ var app = new Framework7({
       {path: '/ingredientes/',url: 'ingredientes.html',},
       {path: '/perfil/',url: 'perfil.html',},
       {path: '/recetas/',url: 'recetas.html',},
-      {path: '/CARrecetas/',url: 'CARrecetas.html',},
+      {path: '/masrecetas/',url: 'masrecetas.html',},
       {path: '/registro/',url: 'registro.html',},
       
     ]
@@ -35,55 +35,62 @@ $$(document).on('deviceready', function() {
     console.log("Device is ready!");
 });
 
-
 // Option 1. Using one 'page:init' handler for all pages
 $$(document).on('page:init', function (e) {
     // Do something here when page loaded and initialized
-    console.log(e);
 })
-
-
 
 $$(document).on('page:init', '.page[data-name="index"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
     console.log("inicio index")
     $$("#inic").on("click", iniciar);
-    $$("#pasr").on("click", pasreg);
+})
+
+$$(document).on('page:init', '.page[data-name="registro"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log(e);
+    console.log('pag registro cargada');
+    $$("#registrar").on("click", autenticar)
 })
 
 
 // Option 2. Using live 'page:init' event handlers for each page
 $$(document).on('page:init', '.page[data-name="prin"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
+    //console.log(e);
     console.log('pag principal cargada');
-    //capacidad()
 })
 
 
 
 
-// Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="registro"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    console.log('pag registro cargada');
-    $$("#registrar").on("click", autenticar)
-    $$("#registrar").on("click", regbd)
-})
 
 $$(document).on('page:init', '.page[data-name="ingredientes"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
+    //Do something here when page with data-name="about" attribute loaded and initialized
+    //console.log(e);
     console.log('pag ingredientes cargada');
 
     console.log(rol)
     if (rol == "admin") {
         $$("#admincar").removeClass('oculto').addClass('visible');
     }
-
     $$("#admnag").on("click", Ningrediente);
+})
 
+
+$$(document).on('page:init', '.page[data-name="misrecetas"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    //console.log(e);
+    console.log('misrecetas cargada');
+})
+
+
+$$(document).on('page:init', '.page[data-name="masrecetas"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    //console.log(e);
+    console.log('masrecetas cargada');
+    $$("#AGpaso").on("click",maspaso);
+    $$("#AGing").on("click",masing);
 })
 
 
@@ -96,16 +103,20 @@ var cont1
 var cont2
 var usuario
 var rol
+//-----
+var num = 0
+var receta =""
+var ingrec =""
+//-----
 var db = firebase.firestore()
 var Refusuario = db.collection("usuarios");//referencia a usuario 
 var Refingredientes = db.collection("ingrediente");//referencia a usuario 
 
 //---------------------------------------------------------------------------------------
 
-//mis fun
+//mis fun--------------------------------------------------------------------------------
 
 function iniciar(){
-        // cada un@ pone su magia para recuperar el mail y la clave de un form...
     email = $$("#iEmail").val();
     var passDelUser = $$("#iCont").val();
 
@@ -113,9 +124,7 @@ function iniciar(){
     .then((userCredential) => {
     // Signed in
     var user = userCredential.user;
-
-
-///----------------    
+    
 
     console.log("Bienvenid@!!! " + email );
     
@@ -147,36 +156,43 @@ function iniciar(){
 
 }
 
-
-function pasreg(){
-    mainView.router.navigate('/registro/');
-
-}
-
-
 function autenticar(){
     email = $$("#rEmail").val();
     cont1 = $$("#c1").val();
     cont2 = $$("#c2").val();
-
-
+    
     if (cont1==cont2) {
         console.log("contraseñas validas")
         password = cont1 
-    
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .then((userCredential) => {
+        .then((userCredential) => {
             // Signed in
             var user = userCredential.user;
             //console.log("usuario creado email:"email+" id:"userCredential )
             // ...
+            var usID = $$("#rEmail").val()
+            var apellido = $$("#rApellido").val()
+            var nombre = $$("#rNombre").val()
+            //carga de datos a bd de usuarios
+            var data = {
+                nombre: nombre,
+                apellido: apellido,
+                rol: "usuario",
+            };
+            Refusuario.doc(usID).set(data)
+            .then(function() { // .then((docRef) => {
+                console.log("OK!");
+            })
+            .catch(function(error) { // .catch((error) => {
+                console.log("Error es de db: " + error);
+            });
         })
-          .catch((error) => {
+        .catch((error) => {
             var errorCode = error.code;
             var errorMessage = error.message;
             console.log("error"+ errorMessage);
         // ..
-      });
+        });
     mainView.router.navigate('/index/');
     }else   {
         $$("#diferen").html("las contraseñas no coinciden")
@@ -184,41 +200,33 @@ function autenticar(){
 }
 
 
+//cargar recetas -----------------------------------------------------------------
 
-// base de datos 
+function maspaso (){
+    var paso = ""
+    num++
+    paso = $$("#elpaso").val()
+    $$("#CrecP").append(num + "-  "+ paso +"<br>")
+    //receta = $$('#CrecP').text();
+    $$("#elpaso").val("")
+    //console.log(receta)
+    //los comentados hay que moverlos para cuando se cargue la reseta en la bd
 
-function regbd() {
-//var datos usuario    
-    var usID = $$("#rEmail").val()
-    var apellido = $$("#rApellido").val()
-    var nombre = $$("#rNombre").val()
-
-//carga de datos a bd de usuarios
-    var data = {
-            nombre: nombre,
-            apellido: apellido,
-            rol: "usuario",
-    };
-
-    Refusuario.doc(usID).set(data)
-    .then(function() { // .then((docRef) => {
-    console.log("OK!");
-    })
-    .catch(function(error) { // .catch((error) => {
-    console.log("Error es de db: " + error);
-    });
 }
 
+function masing() {
+    var ing = $$("#eling").val()
+    $$("#Cing").append(ing + " / ")
+    $$("#eling").val("")
 
-//funciones para admins 
+}
+
+//funciones para admins ---------------------------------------------------------
 
 function Ningrediente(){
 
     var ingrediente = $$("#ingds").val()
     var unidad = $$("#uni").val()
-
-    //cargar ingredientes en bd
-
     var data = {
             unidad: unidad,
     };
